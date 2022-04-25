181cee62a19311f686559c4a44c32d68f55789d5
sonia <sonia.thabouti@ensi-uma.tn>
2022-04-25 13:23:19 +0200

    adding css to the login and sign in forms

--- quizApp/src/app/app-routing/routes.ts
+++ quizApp/src/app/app-routing/routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { QuestionComponent } from '../question/question.component';
 import { HomeComponent } from '../home/home.component';
 import { LoginComponent } from '../signing/login/login.component';
+import { SignInComponent } from '../signing/sign-in/sign-in.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
     {path:'question',component:QuestionComponent},
     {path:'login', component:LoginComponent},
+    {path:'sign-in', component:SignInComponent},
 ];
